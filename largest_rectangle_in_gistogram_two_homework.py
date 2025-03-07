2fcca235c6c1172de970bd6fe56e54dc9850662d
glebdorohov <dorohovgleb1709@yandex.ru>
2025-03-08 02:23:50 +0500

    second homework three task

--- largest_rectangle_in_gistogram_two_homework.py
+++ largest_rectangle_in_gistogram_two_homework.py
@@ -2,12 +2,12 @@ class Solution:
     def largestRectangleArea(self, height):
         stack = []
         answer = 0
+        height += [0]
         for i in range(len(height)):
             while stack and height[i] <= height[stack[-1]]:
-                h,w = height[stack.pop()], i
+                h, w = height[stack.pop()], i
                 if stack:
                     w = i - stack[-1] - 1
                 answer = max(answer, h*w)
-                stack.pop()
             stack.append(i)
         return answer

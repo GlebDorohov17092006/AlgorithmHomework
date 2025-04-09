320eabdf52ae7ff5bef36de89348cc724a28aec4
glebdorohov <dorohovgleb1709@yandex.ru>
2025-04-09 22:29:33 +0500

    four homework

---
+++ second_task_4_homework.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+n = int(input())
+ma = []
+for i in range(n):
+    x, y = map(int, input().split())
+    ma.append([x,y])
+ma.sort()
+print("Ответ:")
+for j in range(n):
+    print(*ma[j])
+
+
+'''Дополнительная визуализация'''
+plt.plot([ma[i][0] for i in range(n)], [ma[i][1] for i in range(n)], marker='o', linestyle='-', color='b')
+plt.show()

1b7ad1dc622061738ab9124ebf280c188c4c4b81
GlebDorohov17092006 <dorokhov.gv@phystech.edu>
2025-11-18 03:51:13 +0500

    Delete backend directory

--- backend/positional_equal.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "positional.h"
-
-struct PositionalEqual
-{
-    bool operator()(Positional *a, Positional *b) const;
-};
--- backend/positional_hash.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "positional.h"
-
-struct PositionalHash
-{
-    size_t operator()(Positional *pos) const;
-};
